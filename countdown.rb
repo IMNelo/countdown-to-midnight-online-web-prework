7919d98b35b7bdd5060310588d2899bf5740a0fa
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-24 03:04:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,10 @@
 def countdown
 
 number = 10
-while number >=10
+while number <=10
 puts "#{number} SECOND(S)!"
-sleep (0.1)
+sleep (0.9)
 number -= 1
-
+break if number == 0
 end
+puts "HAPPY NEW YEAR!"
